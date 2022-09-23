65380582a0880c22b1d4fcb1d44a558c16d409e3
issdilshod <dilshod@themembers.com>
2022-09-23 15:03:46 +0300

    setting up swagger

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,31 +7,136 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+
+    /**     @OA\GET(
+      *         path="/api/user",
+      *         operationId="get_users",
+      *         tags={"User"},
+      *         summary="Get users",
+      *         description="Get users",
+      *             @OA\Response(response=200, description="Successfully"),
+      *             @OA\Response(response=400, description="Bad request"),
+      *             @OA\Response(response=401, description="Not Authorized"),
+      *             @OA\Response(response=404, description="Resource Not Found"),
+      *     )
+      */
     public function index()
     {
         
     }
 
-    
+    /**     @OA\POST(
+      *         path="/api/user",
+      *         operationId="create_user",
+      *         tags={"User"},
+      *         summary="Create user",
+      *         description="Create user",
+      *             @OA\RequestBody(
+      *                 @OA\JsonContent(),
+      *                 @OA\MediaType(
+      *                     mediaType="multipart/form-data",
+      *                     @OA\Schema(
+      *                         type="object",
+      *                         required={"username", "password", "first_name", "last_name"},
+      *                         @OA\Property(property="username", type="text"),
+      *                         @OA\Property(property="password", type="text"),
+      *                         @OA\Property(property="first_name", type="text"),
+      *                         @OA\Property(property="last_name", type="text")
+      *                     ),
+      *                 ),
+      *             ),
+      *             @OA\Response(response=200, description="Successfully"),
+      *             @OA\Response(response=400, description="Bad request"),
+      *             @OA\Response(response=401, description="Not Authorized"),
+      *             @OA\Response(response=404, description="Resource Not Found"),
+      *     )
+      */
     public function store(Request $request)
     {
         
     }
 
-
+    /**     @OA\GET(
+      *         path="/api/user/{id}",
+      *         operationId="get_user",
+      *         tags={"User"},
+      *         summary="Get user",
+      *         description="Get user",
+      *             @OA\Parameter(
+      *                 name="id",
+      *                 in="path",
+      *                 description="user id",
+      *                 @OA\Schema(type="integer"),
+      *                 required=true
+      *             ),
+      *             @OA\Response(response=200, description="Successfully"),
+      *             @OA\Response(response=400, description="Bad request"),
+      *             @OA\Response(response=401, description="Not Authorized"),
+      *             @OA\Response(response=404, description="Resource Not Found"),
+      *     )
+      */
     public function show(User $user)
     {
         
     }
 
-
+    /**     @OA\PUT(
+      *         path="/api/user/{id}",
+      *         operationId="update_user",
+      *         tags={"User"},
+      *         summary="Update user",
+      *         description="Update user",
+      *             @OA\Parameter(
+      *                 name="id",
+      *                 in="path",
+      *                 description="user id",
+      *                 @OA\Schema(type="integer"),
+      *                 required=true
+      *             ),
+      *             @OA\RequestBody(
+      *                 @OA\JsonContent(),
+      *                 @OA\MediaType(
+      *                     mediaType="multipart/form-data",
+      *                     @OA\Schema(
+      *                         type="object",
+      *                         required={},
+      *                         @OA\Property(property="username", type="text"),
+      *                         @OA\Property(property="password", type="text"),
+      *                         @OA\Property(property="first_name", type="text"),
+      *                         @OA\Property(property="last_name", type="text")
+      *                     ),
+      *                 ),
+      *             ),
+      *             @OA\Response(response=200, description="Successfully"),
+      *             @OA\Response(response=400, description="Bad request"),
+      *             @OA\Response(response=401, description="Not Authorized"),
+      *             @OA\Response(response=404, description="Resource Not Found"),
+      *     )
+      */
     public function update(Request $request, User $user)
     {
         
     }
 
-    
+    /**     @OA\DELETE(
+      *         path="/api/user/{id}",
+      *         operationId="delete_user",
+      *         tags={"User"},
+      *         summary="Delete user",
+      *         description="Delete user",
+      *             @OA\Parameter(
+      *                 name="id",
+      *                 in="path",
+      *                 description="user id",
+      *                 @OA\Schema(type="integer"),
+      *                 required=true
+      *             ),
+      *             @OA\Response(response=200, description="Successfully"),
+      *             @OA\Response(response=400, description="Bad request"),
+      *             @OA\Response(response=401, description="Not Authorized"),
+      *             @OA\Response(response=404, description="Resource Not Found"),
+      *     )
+      */
     public function destroy(User $user)
     {
         
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('api/user', [UserController::class, 'index']);
+Route::post('api/user', [UserController::class, 'store']);
+Route::get('api/user/{id}', [UserController::class, 'show']);
+Route::put('api/user/{id}', [UserController::class, 'update']);
+Route::delete('api/user/{id}', [UserController::class, 'destroy']);
