42bf10386558276648e0a0e8c98bca39d85fff86
issdilshod <dilshod@themembers.com>
2022-09-23 20:49:25 +0300

    init controller account (login/logout/is_auth)

--- app/Services/UserAccessTokenService.php
+++ app/Services/UserAccessTokenService.php
@@ -2,6 +2,13 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use App\Models\UserAccessToken;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Str;
+
 /**
  * User access token service class
  * 
@@ -9,5 +16,37 @@ namespace App\Services;
  */
 class UserAccessTokenService {
 
+    /**
+     * Return access token for user
+     * 
+     * @param   Request
+     * @param   Array
+     * @return  UserAccessToken
+     */
+    public function create_access_token(Request $request, $user)
+    {
+        $user = User::where('status', Config::get('custom.status.active'))
+                        ->where('username', $user['username'])
+                        ->where('password', $user['password'])
+                        ->first();
+        $expires_at = Carbon::now(); $expires_at = $expires_at->addDays(1)->toDateTimeString(); // after day expires
+        $entity = [
+            'user_id' => $user['id'],
+            'token' => Str::random(25),
+            'expires_at' => $expires_at,
+        ];
+        $access_token = UserAccessToken::create($entity);
+        return $access_token;
+    }
 
+    /**
+     * Delete user access token
+     * 
+     * @param   Array
+     * @return  void
+     */
+    public function delete_user_access_token($user_access_token)
+    {
+        UserAccessToken::where('token', $user_access_token['token'])->update(['status' => Config::get('custom.status.delete')]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('api/user', UserController::class);
+
+Route::post('api/login', [AccountController::class, 'login']);
+Route::post('api/logout', [AccountController::class, 'logout']);
+Route::get('api/is_auth', [AccountController::class, 'is_auth']);
