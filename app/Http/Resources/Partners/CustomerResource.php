6b2f991ce66e8af43f3bfbfda9003d03efa27931
issdilshod <dilshod@themembers.com>
2022-11-02 12:55:42 +0300

    customers where use on customer get

--- app/Http/Resources/Partners/CustomerResource.php
+++ app/Http/Resources/Partners/CustomerResource.php
@@ -16,7 +16,8 @@ class CustomerResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'name' => $this->name
+            'name' => $this->name,
+            'use' => $this->use
         ];
     }
 }
--- app/Services/Partners/CustomerService.php
+++ app/Services/Partners/CustomerService.php
@@ -4,6 +4,7 @@ namespace App\Services\Partners;
 
 use App\Http\Resources\Partners\CustomerResource;
 use App\Models\Partners\Customer;
+use App\Models\Statements\StatementTransaction;
 use Illuminate\Support\Facades\Config;
 
 class CustomerService {
@@ -13,6 +14,9 @@ class CustomerService {
         $customers = Customer::orderBy('created_at', 'DESC')
                                 ->where('status', Config::get('custom.status.active'))
                                 ->paginate(20);
+
+        $customers = $this->where_use($customers);
+
         return CustomerResource::collection($customers);
     }
 
@@ -61,7 +65,11 @@ class CustomerService {
         $customers = Customer::orderBy('created_at', 'DESC')
                             ->where('status', Config::get('custom.status.active'))
                             ->where('name', 'LIKE', '%' . $search . '%')
+                            ->take(20)
                             ->get();
+
+        $customers = $this->where_use($customers);
+
         return CustomerResource::collection($customers);
     }
 
@@ -101,5 +109,62 @@ class CustomerService {
         $customers = Customer::where('status', Config::get('custom.status.active'))
                                 ->get();
         return count($customers);
+    }
+
+    private function where_use($customers)
+    {
+        foreach ($customers AS $key => $value):
+            $uses = StatementTransaction::select('companies.id as company_id', 'companies.name as company_name', 'statement_periods.period', 'transaction_types.name as type_name', 'transaction_types.id as type_id')
+                                            ->groupBy('statement_transactions.type_id')
+                                            ->groupBy('statements.company_id')
+                                            ->join('statement_periods', 'statement_periods.id', '=', 'statement_transactions.period_id')
+                                            ->join('statements', 'statements.id', '=', 'statement_periods.statement_id')
+                                            ->join('companies', 'companies.id', '=', 'statements.company_id')
+                                            ->join('transaction_types', 'transaction_types.id', '=', 'statement_transactions.type_id')
+                                            ->where('statement_periods.status', Config::get('custom.status.active'))
+                                            ->where('statement_transactions.status', Config::get('custom.status.active'))
+                                            ->where('statement_transactions.customer_id', $value['id'])
+                                            ->get();
+
+            // order by company
+            $uses = $uses->toArray();
+            $one_use = [];
+            foreach ($uses AS $key1 => $value1):
+                // find company
+                $exists = false; $exists_index = 0;
+                foreach ($one_use AS $key2 => $value2):
+                    if ($one_use[$key2]['company_id']==$value1['company_id']){
+                        $exists = true;
+                        $exists_index = $key2;
+                        break;
+                    }
+                endforeach;
+
+                // set
+                if (!$exists){
+                    $one_use[] = [ 
+                        'company_name' => $value1['company_name'], 
+                        'company_id' => $value1['company_id'],
+                        'types' => [
+                            [
+                                'type_id' => $value1['type_id'],
+                                'type_name' => $value1['type_name'],
+                                'period' => $value1['period']
+                            ]
+                        ]
+                    ];
+                }else{
+                    $one_use[$exists_index]['types'][] = [
+                        'type_id' => $value1['type_id'],
+                        'type_name' => $value1['type_name'],
+                        'period' => $value1['period']
+                    ];
+                }
+            endforeach;
+            
+            $customers[$key]['use'] = $one_use;
+        endforeach;
+
+        return $customers;
     }
 }
